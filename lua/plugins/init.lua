490add24c79b08201246d133d9464980c3e188ec
Evgeniy Tatarchuk <spik_13@mail.ru>
2025-04-27 22:42:00 +0300

    feat: added noice for cmd/ search/ notifies

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -144,6 +144,36 @@ local plugins = {
     end,
   },
 
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+    config = function()
+      require('noice').setup {
+        lsp = {
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true,
+          },
+        },
+        presets = {
+          -- bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+        cmdline = {
+          enabled = true, -- enable the command line popup
+        },
+      }
+    end,
+  },
+
   -- we use cmp plugin only when in insert mode
   -- so lets lazyload it at InsertEnter event, to know all the events check h-events
   -- completion , now all of these plugins are dependent on cmp, we load them after cmp
